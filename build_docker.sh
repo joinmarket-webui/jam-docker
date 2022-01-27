0050d1a6005f406465351ea5da55a1c8ec90b271
theborakompanioni <theborakompanioni+github@gmail.com>
2022-01-27 15:40:04 +0100

    cleanup

--- build_docker.sh
+++ build_docker.sh
@@ -4,23 +4,11 @@ set -e
 #-----------------------------------------------------------
 # UMBREL
 
-UMBREL_UI_IMAGE_NAME='joinmarket-webui-umbrel'
+UMBREL_UI_IMAGE_NAME='joinmarket-webui-standalone'
 #UMBREL_UI_IMAGE_TAG=$(git rev-parse HEAD)
-UMBREL_UI_IMAGE_TAG='dev'
+UMBREL_UI_IMAGE_TAG='latest'
 
 echo "Building docker image ${UMBREL_UI_IMAGE_NAME}:${UMBREL_UI_IMAGE_TAG}"
 
-docker build --tag "${UMBREL_UI_IMAGE_NAME}:${UMBREL_UI_IMAGE_TAG}" --file Dockerfile.umbrel .
-# and run with: docker run -it --rm -p "8080:80" joinmarket-webui-umbrel:...
-
-#-----------------------------------------------------------
-# DEV
-
-DEV_UI_IMAGE_NAME='joinmarket-webui-dev'
-#UMBREL_UI_IMAGE_TAG=$(git rev-parse HEAD)
-DEV_UI_IMAGE_TAG='dev'
-
-echo "Building docker image ${DEV_UI_IMAGE_NAME}:${DEV_UI_IMAGE_TAG}"
-
-docker build --tag "${DEV_UI_IMAGE_NAME}:${DEV_UI_IMAGE_TAG}" --file Dockerfile.dev .
-# and run with: docker run -it --rm -p "3000:3000" joinmarket-webui-dev:...
+docker build --tag "${UMBREL_UI_IMAGE_NAME}:${UMBREL_UI_IMAGE_TAG}" ./standalone
+# and run with: docker run -it --rm -p "8080:80" joinmarket-webui-standalone:...
--- readme.md
+++ readme.md
@@ -1,1 +1,10 @@
 # jm-docker
+
+
+```sh
+docker-compose -f docker-compose.regtest.yml up
+```
+
+Will start 2 joinmarket-ui container.
+One is a standalone container with ui + server installed,
+the other one is just a the ui.
