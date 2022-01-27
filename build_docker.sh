7a45ef80cb8140a1615e2123e5b64829ed2af577
theborakompanioni <theborakompanioni+github@gmail.com>
2022-01-27 15:57:00 +0100

    cleanup: split between ui-only and standalone image

--- build_docker.sh
+++ build_docker.sh
@@ -2,13 +2,13 @@
 set -e
 
 #-----------------------------------------------------------
-# UMBREL
+# STANDALONE
 
-UMBREL_UI_IMAGE_NAME='joinmarket-webui-standalone'
-#UMBREL_UI_IMAGE_TAG=$(git rev-parse HEAD)
-UMBREL_UI_IMAGE_TAG='latest'
+STANDALONE_UI_IMAGE_NAME='joinmarket-webui-standalone'
+#STANDALONE_UI_IMAGE_TAG=$(git rev-parse HEAD)
+STANDALONE_UI_IMAGE_TAG='latest'
 
-echo "Building docker image ${UMBREL_UI_IMAGE_NAME}:${UMBREL_UI_IMAGE_TAG}"
+echo "Building docker image ${STANDALONE_UI_IMAGE_NAME}:${STANDALONE_UI_IMAGE_TAG}"
 
-docker build --tag "${UMBREL_UI_IMAGE_NAME}:${UMBREL_UI_IMAGE_TAG}" ./standalone
+docker build --tag "${STANDALONE_UI_IMAGE_NAME}:${STANDALONE_UI_IMAGE_TAG}" ./standalone
 # and run with: docker run -it --rm -p "8080:80" joinmarket-webui-standalone:...
