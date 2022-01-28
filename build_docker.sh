24a17f4ac3a602fab3938642b2a00a661153de99
theborakompanioni <theborakompanioni+github@gmail.com>
2022-01-28 16:22:12 +0100

    refactor: provide repo params via build args for ui-only

--- build_docker.sh
+++ build_docker.sh
@@ -2,8 +2,17 @@
 set -e
 
 #-----------------------------------------------------------
-# STANDALONE
+# UI_ONLY
+UI_ONLY_IMAGE_NAME='joinmarket-webui-ui-only'
+#STANDALONE_UI_IMAGE_TAG=$(git rev-parse HEAD)
+UI_ONLY_IMAGE_TAG='latest'
 
+echo "Building docker image ${UI_ONLY_IMAGE_NAME}:${UI_ONLY_IMAGE_TAG}"
+
+docker build --tag "${UI_ONLY_IMAGE_NAME}:${UI_ONLY_IMAGE_TAG}" ./ui-only
+# and run with: docker run -it --rm -p "8080:80" joinmarket-webui-ui-only:...
+
+# STANDALONE
 STANDALONE_UI_IMAGE_NAME='joinmarket-webui-standalone'
 #STANDALONE_UI_IMAGE_TAG=$(git rev-parse HEAD)
 STANDALONE_UI_IMAGE_TAG='latest'
@@ -12,3 +21,4 @@ echo "Building docker image ${STANDALONE_UI_IMAGE_NAME}:${STANDALONE_UI_IMAGE_TA
 
 docker build --tag "${STANDALONE_UI_IMAGE_NAME}:${STANDALONE_UI_IMAGE_TAG}" ./standalone
 # and run with: docker run -it --rm -p "8080:80" joinmarket-webui-standalone:...
+
