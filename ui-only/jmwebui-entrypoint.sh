29cf7a1b80bce80c5f11911412f06fe3a3dc8ce2
theborakompanioni <theborakompanioni+github@gmail.com>
2022-01-28 16:22:12 +0100

    refactor: provide repo params via build args for ui-only

--- ui-only/jmwebui-entrypoint.sh
+++ ui-only/jmwebui-entrypoint.sh
@@ -4,7 +4,7 @@ set -eu
 export JMWEBUI_JM_WALLETD_HOST
 export JMWEBUI_JM_WALLETD_PORT
 
-# due to `set -u` this would fail if variables are not defined
+# due to `set -u` this fails if variables are not defined
 export JMWEBUI_JM_WALLETD_PROXY="https://${JMWEBUI_JM_WALLETD_HOST}:${JMWEBUI_JM_WALLETD_PORT}"
 echo "Will proxy requests for /api/* to ${JMWEBUI_JM_WALLETD_PROXY}/*"
 
