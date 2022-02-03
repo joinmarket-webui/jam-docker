7a59d075be9d01d1f48b03fac751a712dab9bec8
theborakompanioni <theborakompanioni+github@gmail.com>
2022-02-03 22:48:27 +0100

    review: fix double dash after image name prefix

--- build_docker.sh
+++ build_docker.sh
@@ -12,7 +12,7 @@ IMAGE_TAG='latest'
 for context in "${contexts[@]}"
 do
     docker_path="${script_dir}/${context}"
-    image_name="${ORG}/${IMAGE_NAME_PREFIX}-${context}:${IMAGE_TAG}"
+    image_name="${ORG}/${IMAGE_NAME_PREFIX}${context}:${IMAGE_TAG}"
     echo "Building docker image ${image_name}"
 	docker build --label "local" --tag "${image_name}" "${docker_path}"
     echo "Built image! Run with: docker run -it --rm -p \"8080:80\" ${image_name}"
