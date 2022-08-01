17bfcd9c0693983a60878e9bdcadc5f1dc104efd
theborakompanioni <theborakompanioni+github@gmail.com>
2022-08-01 20:44:54 +0200

    dev: move log files from jm dir to /var/log/jam

--- standalone/jam-entrypoint.sh
+++ standalone/jam-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-# ensure 'logs' directory exists
-mkdir --parents "${DATADIR}/logs"
+# ensure 'log' directory exists
+mkdir --parents /var/log/jam
 
 # restore the default config
 if [ ! -f "$CONFIG" ] || [ "${RESTORE_DEFAULT_CONFIG}" = "true" ]; then
