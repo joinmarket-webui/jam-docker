d1dd634bb808688887e2bd32d44e9951b7b8ce41
Thebora Kompanioni <theborakompanioni@users.noreply.github.com>
2022-10-18 23:12:38 +0200

    fix: ensure jm working dir exists (#68)
    * fix: ensure jm working dir exists
    
    * chore: prevent supervisord from searching for its config file
    
    * docs(readme): add common env vars to docker standalone example

--- standalone/jam-entrypoint.sh
+++ standalone/jam-entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e
 
-# ensure 'log' directory exists
+# ensure jm working directory exists
+mkdir --parents "${DATADIR}/"
+
+# ensure log directory exists
 mkdir --parents /var/log/jam
 
 # restore the default config
@@ -107,4 +110,4 @@ if [ "${ENSURE_WALLET}" = "true" ]; then
 fi
 
 
-exec supervisord
+exec supervisord --configuration /etc/supervisor/supervisord.conf
