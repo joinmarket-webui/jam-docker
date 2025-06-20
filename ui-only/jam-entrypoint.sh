79cae4111f8b7154c1927f8b7b73acfabd633ab6
theborakompanioni <theborakompanioni+github@gmail.com>
2025-06-20 19:27:13 +0200

    feat(ui-only): ability to specify ob-watcher host individually

--- ui-only/jam-entrypoint.sh
+++ ui-only/jam-entrypoint.sh
@@ -4,6 +4,7 @@ set -eu
 export JAM_JMWALLETD_HOST
 export JAM_JMWALLETD_API_PORT
 export JAM_JMWALLETD_WEBSOCKET_PORT
+export JAM_JMOBWATCH_HOST
 export JAM_JMOBWATCH_PORT
 
 # due to `set -u` this fails if variables are not defined
@@ -16,7 +17,7 @@ JAM_JMWALLETD_WEBSOCKET_PROXY="${JAM_JMWALLETD_HOST}:${JAM_JMWALLETD_WEBSOCKET_P
 echo "Will proxy requests for /jmws to ${JAM_JMWALLETD_WEBSOCKET_PROXY}/"
 
 export JAM_JMOBWATCH_PROXY
-JAM_JMOBWATCH_PROXY="${JAM_JMWALLETD_HOST}:${JAM_JMOBWATCH_PORT}"
+JAM_JMOBWATCH_PROXY="${JAM_JMOBWATCH_HOST:-$JAM_JMWALLETD_HOST}:${JAM_JMOBWATCH_PORT}"
 echo "Will proxy requests for /obwatch/* to ${JAM_JMOBWATCH_PROXY}/"
 
 # pass on to the original nginx entry point
