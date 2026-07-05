ee2a5d98ca62ca489208958c7e2c5e877867aa08
theborakompanioni <theborakompanioni+github@gmail.com>
2026-07-05 13:01:52 +0200

    chore(build): default obwatcher host to 127.0.0.1

--- standalone-ng/jam-ng-entrypoint.sh
+++ standalone-ng/jam-ng-entrypoint.sh
@@ -62,9 +62,9 @@ if [ -n "${JAM_UI_PORT##*[!0-9]*}" ]; then
     sed --in-place "s/listen 80;/listen ${JAM_UI_PORT};/g" /etc/nginx/conf.d/default.conf
     sed --in-place "s/listen \[::\]:80;/listen [::]:${JAM_UI_PORT};/g" /etc/nginx/conf.d/default.conf
 fi
-if [ -n "${ORDERBOOK_WATCHER__HTTP_PORT##*[!0-9]*}" ]; then
-    echo "Proxy obwatcher requests to 127.0.0.1:${ORDERBOOK_WATCHER__HTTP_PORT}."
-    sed --in-place "s/server 127.0.0.1:8000;/server 127.0.0.1:${ORDERBOOK_WATCHER__HTTP_PORT};/g" /etc/nginx/conf.d/default.conf
+if [ -n "${ORDERBOOK_WATCHER__HTTP_HOST}" ] && [ -n "${ORDERBOOK_WATCHER__HTTP_PORT##*[!0-9]*}" ]; then
+    echo "Proxy obwatcher requests to ${ORDERBOOK_WATCHER__HTTP_HOST}:${ORDERBOOK_WATCHER__HTTP_PORT}."
+    sed --in-place "s/server 127.0.0.1:8000;/server ${ORDERBOOK_WATCHER__HTTP_HOST}:${ORDERBOOK_WATCHER__HTTP_PORT};/g" /etc/nginx/conf.d/default.conf
 fi
 
 # wait for a ready file before starting services (e.g. chain sync gate)
